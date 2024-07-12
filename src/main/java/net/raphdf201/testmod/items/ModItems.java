f5c46fa5df18301d0b2a3062026068f1b29ef70f
Raphdf201 <deschenesr545@gmail.com>
2024-07-12 10:04:28 -0400

    Finished adding sword

--- src/main/java/net/raphdf201/testmod/items/ModItems.java
+++ src/main/java/net/raphdf201/testmod/items/ModItems.java
@@ -15,6 +15,22 @@ import net.raphdf201.testmod.TestMod;
 import static net.raphdf201.testmod.TestMod.MODID;
 
 public class ModItems {
+	public static final FoodComponent SUS_THINGS = new FoodComponent.Builder()
+			.alwaysEdible()
+			.snack()
+			.nutrition(696)
+			.saturationModifier(6969)
+			.statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 696969, 2, false, false, false), 1)
+			.build();
+	// NORMAL ITEMS
+	public static final Item SUS_SUBSTANCE = register(
+			new Item(new Item.Settings().maxCount(TestMod.ModMaxCount).fireproof().food(SUS_THINGS)), "sus_substance"
+	);
+	// TOOLS/WEAPONS ITEMS
+	public static final Item PLASTIC_SWORD = register(
+			new SwordItem(PlasticMaterial.INSTANCE, new Item.Settings()), "plastic_sword"
+	);
+
 	public static Item register(Item item, String id) {
 		Identifier itemID = Identifier.of(MODID, id);
 		return Registry.register(Registries.ITEM, itemID, item);
@@ -23,25 +39,7 @@ public class ModItems {
 	public static void initialize() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
 				.register((itemGroup) -> itemGroup.add(SUS_SUBSTANCE));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+				.register((itemGroup) -> itemGroup.add(PLASTIC_SWORD));
 	}
-
-	public static final FoodComponent SUS_THINGS = new FoodComponent.Builder()
-			.alwaysEdible()
-			.snack()
-			.nutrition(696)
-			.saturationModifier(6969)
-			.statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 696969, 2, false, false, false), 1)
-			.build();
-
-
-	// NORMAL ITEMS
-	public static final Item SUS_SUBSTANCE = register(
-			new Item(new Item.Settings().maxCount(TestMod.ModMaxCount).fireproof().food(SUS_THINGS)), "sus_substance"
-	);
-
-
-	// TOOLS/WEAPONS ITEMS
-	public static final Item PLASTIC_SWORD = register(
-			new SwordItem(PlasticMaterial.INSTANCE, new Item.Settings()), "plastic_sword"
-	);
 }
--- src/main/java/net/raphdf201/testmod/items/PlasticMaterial.java
+++ src/main/java/net/raphdf201/testmod/items/PlasticMaterial.java
@@ -19,7 +19,7 @@ public class PlasticMaterial implements ToolMaterial {
 
 	@Override
 	public float getAttackDamage() {
-		return 0;
+		return 0.5f;
 	}
 
 	@Override
